fd7bb4b7e2ea0628f7969b61b952a41e461aa12c
Yash Bhalekar <yashbhalekar0@gmail.com>
2024-09-24 00:54:31 +0530

    Update download.php

--- download.php
+++ download.php
@@ -1,5 +1,5 @@
 <?php
-$file = 'https://github.com/yashbhalekar/Resume/raw/main/Yash_Bhalekar_Resume.pdf';
+$file = 'https://github.com/yashbhalekar/Resume/raw/main/Yash_Bhalekar_Resume(1).pdf';
 header('Content-Type: application/pdf');
 header('Content-Disposition: attachment; filename="Yash_Bhalekar_Resume.pdf"');
 readfile($file);
